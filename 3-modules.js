08e8249232b9dec030a93386db6dfc2651cdbe6e
kwonjeomsim <kwonjumsem@gmail.com>
2022-08-09 10:46:41 +0900

    firstcommit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mine-grenade");
+sayHi("kwon");
+sayHi(names.john);
+sayHi(names.tom);
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = "SUPER SECRET";
+// share
+const john = "john";
+const tom = "tom";
+
+console.log(module);
+
+module.exports = { john, tom };
